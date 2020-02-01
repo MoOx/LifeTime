f5eb6ca66daa39e26aab8a07ce2b94b87c7763a5
Maxime Thirouin <git@moox.io>
2020-02-01 23:15:01 +0100

    Remove eslint styles config
    Ref https://github.com/facebook/react-native/issues/26903

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community/eslint-config', 'eslint-config-react-app'],
+  extends: [
+    '@react-native-community/eslint-config',
+    'eslint-config-react-app',
+    'eslint-config-prettier',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+  },
 };
