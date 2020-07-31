321a82d0271c8d1ab192ddd1d63535d6639e8e8b
Maxime Thirouin <git@moox.io>
2020-07-31 11:00:00 +0200

    Update sentry setup

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
+import * as Sentry from '@sentry/react-native';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+if (!__DEV__) {
+  const dsn =
+    'https://aa2bb6c101304c549ba4aa2006f31678@o109907.ingest.sentry.io/5348542';
+  Sentry.init({
+    dsn,
+    enableAutoSessionTracking: true,
+  });
+}
+
+try {
+  AppRegistry.registerComponent(appName, () => App);
+} catch (err) {
+  Sentry.captureException(err);
+}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import 'react-native-gesture-handler';
-
-import * as Sentry from '@sentry/react-native';
-
-Sentry.init({
-  dsn:
-    'https://aa2bb6c101304c549ba4aa2006f31678@o109907.ingest.sentry.io/5348542',
-});
-
 /**
  * Do not modify this file - it is a proxy to your `App.re` file
  * located in the `src/` folder.
