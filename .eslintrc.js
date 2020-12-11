0f1bd212229ecf4aa5645799f3db69861d929c6c
Maxime Thirouin <git@moox.io>
2020-12-11 16:00:51 +0100

    Lint all js files (including .bs.js)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community/eslint-config',
-    'eslint-config-react-app',
-    'eslint-config-prettier',
-  ],
+  extends: ['@react-native-community/eslint-config', 'eslint-config-prettier'],
   rules: {
     'prettier/prettier': 0,
+    // not interesting for rescript generated js
+    'no-shadow': 0,
+    'no-bitwise': 0,
+    'no-useless-escape': 0,
+  },
+  env: {
+    'jest/globals': true,
   },
 };
